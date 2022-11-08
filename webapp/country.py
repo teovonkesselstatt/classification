12828746b95e38582130dc294663131e42c9c7be
teovonkesselstatt <54458556+teovonkesselstatt@users.noreply.github.com>
2022-11-07 21:14:59 -0300

    Update country.py

--- webapp/country.py
+++ webapp/country.py
@@ -65,8 +65,8 @@ def run_app():
     st.table(df_temp.sort_values(by=['Year'], ascending=False))
 
     st.sidebar.markdown('### Legend:')
-    st.sidebar.write('4-way Classification: Fix, Crawling Peg, Dirty Float, Float, OVM (Unclassified One Variable Missing), NON (non-existing or non-independent country), Not Classified: Undisclosed Basket or too little volatility in variables.')
-    st.sidebar.write('3-way Classification: Fix, Interm, Float, OVM, NON, Not Classified (Basket), Not Classified: (Low Volatility).')
+    st.sidebar.write('4-way Classification: Fix, Crawling Peg, Dirty Float, Float, OVM (Unclassified One Variable Missing), NON (non-existing or non-independent country), Not Classified: Undisclosed Basket or too little volatility in variables (Low Volatility).')
+    st.sidebar.write('3-way Classification: Fix, Interm, Float, OVM, NON, Undisclosed Basket, Low Volatility.')
     st.sidebar.write('*Industrial Countries')
     st.sidebar.write('**Emerging Economies')
     st.sidebar.write('#### Reference:')
